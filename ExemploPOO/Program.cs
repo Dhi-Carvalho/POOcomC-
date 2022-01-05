e69680003435f3e0b35f16db3f11f55381ac6f80
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2022-01-04 21:45:34 -0300

    Exemplo de encapsulamento em POO com C#

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -7,12 +7,24 @@ namespace ExemploPOO
   {
     static void Main(string[] args)
     {
+      //Exemplo de Abstração
       Pessoa p1 = new Pessoa();
 
       p1.Nome = "Bob";
       p1.Idade = 20;
 
       p1.Apresentar();
+
+      //Exemplo de Encapsulamento
+      // Valores válidos
+      Retangulo r = new Retangulo();
+      r.DefinirMedidas(30, 30);
+      System.Console.WriteLine($"Área do retângulo: {r.ObterArea()}");
+
+      // Valores inválidos
+      Retangulo r2 = new Retangulo();
+      r2.DefinirMedidas(0, 0);
+      System.Console.WriteLine($"Área do retângulo: {r2.ObterArea()}");
     }
   }
 }
