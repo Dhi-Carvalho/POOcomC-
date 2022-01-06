f559e8296797bfa09e7dedcd1b284f4560b787ee
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2022-01-05 22:36:29 -0300

    Inserido os exemplos práticos de Herança e dos tipos de Polimorfismo

---
+++ ExemploPOO/Models/Professor.cs
@@ -0,0 +1,11 @@
+namespace ExemploPOO.Models
+{
+  public class Professor : Pessoa
+  {
+    public double Salario { get; set; }
+    public override void Apresentar()
+    {
+      System.Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos e meu salário é de R$ {Salario}.");
+    }
+  }
+}
--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -25,6 +25,46 @@ namespace ExemploPOO
       Retangulo r2 = new Retangulo();
       r2.DefinirMedidas(0, 0);
       System.Console.WriteLine($"Área do retângulo: {r2.ObterArea()}");
+
+      //Exemplos de Herança
+      //Classe Aluno herdando de Pessoa
+      Aluno p3 = new Aluno();
+      p3.Nome = "Diogo";
+      p3.Idade = 36;
+      p3.Nota = 10;
+
+      p3.Apresentar();
+      System.Console.WriteLine($"Minha nota foi: {p3.Nota}");
+
+      //Classe Professor herdando de Pessoa
+      Professor p4 = new Professor();
+      p4.Nome = "Ismael";
+      p4.Idade = 68;
+      p4.Salario = 1200.00;
+
+      p4.Apresentar();
+      System.Console.WriteLine($"Meu salário é R$ {p4.Salario}");
+
+      //Exemplos de Polimorfismo
+      //Polimorfismo em tempo de execução
+      Aluno p5 = new Aluno();
+      p5.Nome = "Diogo";
+      p5.Idade = 36;
+      p5.Nota = 10;
+
+      p5.Apresentar();
+
+      Professor p6 = new Professor();
+      p6.Nome = "Ismael";
+      p6.Idade = 68;
+      p6.Salario = 1200.00;
+
+      p6.Apresentar();
+
+      //Polimorfismo em tempo de compilação
+      Calculadora calc = new Calculadora();
+      System.Console.WriteLine("Resultado da primeira soma: " + calc.Somar(5, 10));
+      System.Console.WriteLine("Resultado da segunda soma: " + calc.Somar(6, 10, 5));
     }
   }
 }
